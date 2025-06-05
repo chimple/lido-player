bf8bd68dccfd73c2b3e0a97fb460211c4a6d8ee4
karansamritkar <karan.samritkar@sutara.org>
2025-06-05 14:38:58 +0530

    added object in storybook

--- src/stories/Templates/puzzleGame/puzzleGame.stories.ts
+++ src/stories/Templates/puzzleGame/puzzleGame.stories.ts
@@ -1,27 +1,55 @@
 import { Meta, StoryObj } from '@storybook/web-components';
 import { html } from 'lit';
 
-type PuzzleArgs = {
-  image1: string;
-  image2: string;
-  backgroundImage: string;
+type DraggableImage = {
+  id: string;
+  value: string;
+  src: string;
+  width?: string;
 };
 
-const defaultImage1 = "https://aeakbcdznktpsbrfsgys.supabase.co/storage/v1/object/public/template-assets/puzzles-asset/gaezzle1.png";
-const defaultImage2 = "https://aeakbcdznktpsbrfsgys.supabase.co/storage/v1/object/public/template-assets/puzzles-asset/gaezzle2.png";
+type DropCell = {
+  id: string;
+  value: string;
+  x: string;
+  y: string;
+};
+
+type PuzzleArgs = {
+  backgroundImage: string;
+  draggables: DraggableImage[];
+  dropCells: DropCell[];
+};
+
 const defaultBackground = "https://aeakbcdznktpsbrfsgys.supabase.co/storage/v1/object/public/template-assets/puzzles-asset/gaezzle_bg.jpg";
 
 const meta: Meta<PuzzleArgs> = {
   title: 'Templates/PuzzleGame',
   argTypes: {
-    image1: { control: 'text', name: 'Image 1 URL' },
-    image2: { control: 'text', name: 'Image 2 URL' },
     backgroundImage: { control: 'text', name: 'Background Image URL' },
+    draggables: { control: 'object', name: 'Draggable Images' },
+    dropCells: { control: 'object', name: 'Drop Cells' },
   },
   args: {
-    image1: defaultImage1,
-    image2: defaultImage2,
     backgroundImage: defaultBackground,
+    draggables: [
+      {
+        id: 'image1',
+        value: 'option1',
+        src: 'https://aeakbcdznktpsbrfsgys.supabase.co/storage/v1/object/public/template-assets/puzzles-asset/gaezzle1.png',
+        width: '313px',
+      },
+      {
+        id: 'image2',
+        value: 'option2',
+        src: 'https://aeakbcdznktpsbrfsgys.supabase.co/storage/v1/object/public/template-assets/puzzles-asset/gaezzle2.png',
+        width: '269px',
+      },
+    ],
+    dropCells: [
+      { id: 'drop1', value: 'option1', y: '35.3%', x: '44.8%' },
+      { id: 'drop2', value: 'option2', y: '57.2%', x: '42.7%' },
+    ],
   },
 };
 
@@ -35,16 +63,26 @@ export const PuzzleGame: StoryObj<PuzzleArgs> = {
 };
 
 function getContainerXml(args: PuzzleArgs) {
+  const draggableImagesXml = args.draggables
+    .map((img) => {
+      return `<lido-image id="${img.id}" width="${img.width || '250px'}" disableEdit="true" value="${img.value}" visible="true" type="drag" src="${img.src}" z="0" altText="{${img.id}}"></lido-image>`;
+    })
+    .join('\n');
+
+  const dropCellsXml = args.dropCells
+    .map((cell, index) => {
+      return `<lido-cell id="${cell.id}" value="${cell.value}" tabIndex="${index + 2}" height="10%" width="10%" layout="pos" y="${cell.y}" x="${cell.x}" visible="true" type="drop" bgColor="transparent"></lido-cell>`;
+    })
+    .join('\n');
+
   return `<main>
   <lido-container id="lido-container" tabIndex="1" isContinueOnCorrect="true" onCorrect="lido-avatar.avatarAnimate='Success'; this.sleep='2000';" showCheck="false" visible="true">
     <lido-image id="mainImage" disableEdit="true" value="background-image" visible="true" src="${args.backgroundImage}" z="0" onEntry="" altText="{backgroundImage}"></lido-image>
     <lido-cell layout="random" visible="true" width="1200px" height="100px" y="65%">
-      <lido-image id="image1" width="313px" disableEdit="true" value="option1" visible="true" type="drag" src="${args.image1}" z="0" altText="{Image1}"></lido-image>
-      <lido-image id="image2" width="269px" disableEdit="true" value="option2" visible="true" type="drag" src="${args.image2}" z="0" altText="{Image2}"></lido-image>
+      ${draggableImagesXml}
     </lido-cell>
 
-    <lido-cell id="drop1" value="option1" tabIndex="2" height="10%" width="10%" onEntry="this.position='absolute'" y="35.2%" x="44.9%" visible="true" type="drop"></lido-cell>
-    <lido-cell id="drop2" value="option2" tabIndex="3" height="10%" width="10%" onEntry="this.position='absolute'" y="57%" x="42.75%" visible="true" type="drop"></lido-cell>
+    ${dropCellsXml}
   </lido-container>
 </main>`;
 }
