a9e726a37bd81e3a1f21a998e12c6ad45b3c84ac
bhanugummana <bhanugummana@users.noreply.github.com>
2025-06-26 06:31:22 +0000

    deploy: 6e5958f4b99b491ffe682ffc74bd8d78d1f82a80

--- storybook/assets/flash-card.stories-C9KQ4eMs.js
+++ storybook/assets/flash-card.stories-C9KQ4eMs.js
@@ -39,10 +39,10 @@ import"./lit-element-BxxbMms4.js";import{x as r}from"./lit-html-DLYuhZ0i.js";con
         <lido-container id="lido-container" tabIndex="1" value="mainContainer3" objective="lion,crocodile,cow" bgColor="#B1BEFD" ariaLabel="" height="100vh" width="100vw" x="0" y="0" z="0" visible="true" audio="background1.mp3" onTouch="" onCorrect="lido-avatar.avatarAnimate='Success'; this.sleep='2000'; this.removeClone='.cloned-element'" onEntry="" onInCorrect="lido-avatar.avatarAnimate='Fail'; this.sleep='2000';" showCheck="false" isContinueOnCorrect="true" dropAttr="diagonal">
             <lido-row visible="true" width="landscape.100%, portrait.338px" onEntry="" height="landscape.100%, portrait." y="landscape.340px, portrait.300px" x="landscape.,portrait.520px" direction="landscape.row, portrait.column">
                 <lido-flash-card x="50" y="200" width="220px" height="140px">
-                            <lido-image visible="true" slot="front" src="https://aeakbcdznktpsbrfsgys.supabase.co/storage/v1/object/public/thumbnails/sticker/Bird_Blue.png" z="0" width="200px" height="300px" onEntry="">
-                        </lido-image>
+                    <lido-image visible="true" slot="front" src="https://aeakbcdznktpsbrfsgys.supabase.co/storage/v1/object/public/thumbnails/sticker/Bird_Blue.png" z="0" width="200px" height="300px" onEntry="">
+                    </lido-image>
                     <lido-text slot="back" width="300px" height="200px" display="flex" onEntry="" font="" fontSize="48px" z="1" fontColor="black" fontWeight="bold" color="#000000" string="Bird" visible="true" y="landscape.35px, portrait.15px" bgColor="yellow">
-                      </lido-text>
+                    </lido-text>
                 </lido-flash-card>
             </lido-row>
         </lido-container>
@@ -108,10 +108,10 @@ import"./lit-element-BxxbMms4.js";import{x as r}from"./lit-html-DLYuhZ0i.js";con
         <lido-container id="lido-container" tabIndex="1" value="mainContainer3" objective="lion,crocodile,cow" bgColor="#B1BEFD" ariaLabel="" height="100vh" width="100vw" x="0" y="0" z="0" visible="true" audio="background1.mp3" onTouch="" onCorrect="lido-avatar.avatarAnimate='Success'; this.sleep='2000'; this.removeClone='.cloned-element'" onEntry="" onInCorrect="lido-avatar.avatarAnimate='Fail'; this.sleep='2000';" showCheck="false" isContinueOnCorrect="true" dropAttr="diagonal">
             <lido-row visible="true" width="landscape.100%, portrait.338px" onEntry="" height="landscape.100%, portrait." y="landscape.340px, portrait.300px" x="landscape.,portrait.520px" direction="landscape.row, portrait.column">
                 <lido-flash-card x="50" y="200" width="220px" height="140px">
-                            <lido-image visible="true" slot="front" src="https://aeakbcdznktpsbrfsgys.supabase.co/storage/v1/object/public/thumbnails/sticker/Bird_Blue.png" z="0" width="200px" height="300px" onEntry="">
-                        </lido-image>
+                    <lido-image visible="true" slot="front" src="https://aeakbcdznktpsbrfsgys.supabase.co/storage/v1/object/public/thumbnails/sticker/Bird_Blue.png" z="0" width="200px" height="300px" onEntry="">
+                    </lido-image>
                     <lido-text slot="back" width="300px" height="200px" display="flex" onEntry="" font="" fontSize="48px" z="1" fontColor="black" fontWeight="bold" color="#000000" string="Bird" visible="true" y="landscape.35px, portrait.15px" bgColor="yellow">
-                      </lido-text>
+                    </lido-text>
                 </lido-flash-card>
             </lido-row>
         </lido-container>
--- storybook/assets/trace.stories-CCvPtQG8.js
+++ storybook/assets/trace.stories-CCvPtQG8.js
@@ -1,4 +1,4 @@
-import"./lit-element-BxxbMms4.js";import{x as i}from"./lit-html-DLYuhZ0i.js";const d={title:"Component/Trace",argTypes:{svgFile:{control:"file"},mode:{control:"select",options:["noFlow","showFlow","freeTrace","blindTracing","blindFreeTrace"]}}},a={args:{svgFile:["https://aeakbcdznktpsbrfsgys.supabase.co/storage/v1/object/public/template-assets/trace/hindi_aa.svg"],mode:"showFlow"},render:o=>{const e=l(o);return console.log("🚀 ~ xml:",e),i`<lido-home xml-data="${e}" xmlData="${e}"></lido-home>`}};function l(o){let e=o.svgFile[0],s=o.mode;return`<main>
+import"./lit-element-BxxbMms4.js";import{x as i}from"./lit-html-DLYuhZ0i.js";const m={title:"Component/Trace",argTypes:{svgFile:{control:"file"},mode:{control:"select",options:["noFlow","showFlow","freeTrace","blindTracing","blindFreeTrace"]}}},a={args:{svgFile:["https://aeakbcdznktpsbrfsgys.supabase.co/storage/v1/object/public/template-assets/trace/hindi_aa.svg"],mode:"showFlow"},render:o=>{const e=l(o);return console.log("🚀 ~ xml:",e),i`<lido-home xml-data="${e}" xmlData="${e}"></lido-home>`}};function l(o){let e=o.svgFile[0],s=o.mode;return`<main>
     <lido-container id="lido-container" tabIndex="1" value="mainContainer1" objective="D-6ba518e3,D-5a192ba3,D-b686f882,D-9095b74f,D-e0a32c78,D-1e4e88f1" ariaLabel="" height="100vh" width="100vw" x="0" y="0" z="0" bgColor="#FFB742" visible="true" audio="background1.mp3" onTouch="" onCorrect="lido-avatar.avatarAnimate='Success'; this.sleep='2000';" onEntry="" onInCorrect="lido-avatar.avatarAnimate='Fail'; this.sleep='2000';" showCheck="false" isContinueOnCorrect="true">
         <lido-trace id="image1" tabIndex="2" value="image1" visible="true" svgSource="${e}" mode="${s}" z="0" width="1600px" height="900px" onEntry="" altText="{backgroundImage}">
         </lido-trace>
@@ -9,8 +9,8 @@ import"./lit-element-BxxbMms4.js";import{x as i}from"./lit-html-DLYuhZ0i.js";con
     mode: 'showFlow'
   },
   render: args => {
-    const xml = getContainerXml(args);
+    const xml = getContainerXml(args); // Function to generate XML based on args
     console.log('🚀 ~ xml:', xml);
     return html\`<lido-home xml-data="\${xml}" xmlData="\${xml}"></lido-home>\`;
   }
-}`,...(r=(n=a.parameters)==null?void 0:n.docs)==null?void 0:r.source}}};const g=["QuestionAndOptions"];export{a as QuestionAndOptions,g as __namedExportsOrder,d as default};
+}`,...(r=(n=a.parameters)==null?void 0:n.docs)==null?void 0:r.source}}};const g=["QuestionAndOptions"];export{a as QuestionAndOptions,g as __namedExportsOrder,m as default};
