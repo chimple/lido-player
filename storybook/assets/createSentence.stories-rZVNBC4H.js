a3bca63490e8b274d493b82244b66587c0dabfdb
bhanugummana <bhanugummana@users.noreply.github.com>
2025-07-17 08:51:31 +0000

    deploy: 80299dd1dc59f5b5f1d57af0c2d7147efcb56e18

--- storybook/assets/createSentence.stories-rZVNBC4H.js
+++ storybook/assets/createSentence.stories-rZVNBC4H.js
@@ -23,13 +23,13 @@ import"./lit-element-BxxbMms4.js";import{x as n}from"./lit-html-DLYuhZ0i.js";con
                     </lido-cell>
                 </lido-cell>
                 <lido-cell layout="row" visible="true" width="landscape.70%, portrait.88%" y="landscape.40%, portrait.42%" x="0%" z="2" onEntry="this.position='relative'">
-                    <lido-text id="option1" value="${t.option1}" tab-index="5" flexible-width="false" visible="true" bg-color="#6DC1ED" onEntry="this.borderRadius='25px'; this.boxShadow='unset'; this.fontWeight='800'; this.color='white';" width="150px" height="90px" string="${t.option1}" font-family="'Baloo 2', serif" font-size="24px" z="2" type="drag">
+                    <lido-text id="option1" value="${t.option1}" tab-index="5" flexible-width="false" visible="true" bg-color="#FCF3B1" onEntry="this.borderRadius='25px'; this.boxShadow='unset'; this.fontWeight='800'; " width="150px" height="90px" string="${t.option1}" font-family="'Baloo 2', serif" font-size="24px" z="2" type="drag">
                     </lido-text>
-                    <lido-text id="option2" value="${t.option2}" tab-index="6" flexible-width="false" visible="true" bg-color="#6DC1ED" onEntry="this.borderRadius='25px'; this.boxShadow='unset'; this.fontWeight='800'; this.color='white';" width="150px" height="90px" string="${t.option2}" font-family="'Baloo 2', serif" font-size="24px" z="2" type="drag">
+                    <lido-text id="option2" value="${t.option2}" tab-index="6" flexible-width="false" visible="true" bg-color="#FCF3B1" onEntry="this.borderRadius='25px'; this.boxShadow='unset'; this.fontWeight='800';" width="150px" height="90px" string="${t.option2}" font-family="'Baloo 2', serif" font-size="24px" z="2" type="drag">
                     </lido-text>
-                    <lido-text id="option3" value="${t.option3}" tab-index="7" flexible-width="false" visible="true" bg-color="#6DC1ED" onEntry="this.borderRadius='25px'; this.boxShadow='unset'; this.fontWeight='800'; this.color='white';" width="150px" height="90px" string="${t.option3}" font-family="'Baloo 2', serif" font-size="24px" z="2" type="drag">
+                    <lido-text id="option3" value="${t.option3}" tab-index="7" flexible-width="false" visible="true" bg-color="#FCF3B1" onEntry="this.borderRadius='25px'; this.boxShadow='unset'; this.fontWeight='800'; " width="150px" height="90px" string="${t.option3}" font-family="'Baloo 2', serif" font-size="24px" z="2" type="drag">
                     </lido-text>
-                    <lido-text id="option4" value="${t.option4}" tab-index="8" flexible-width="false" visible="true" bg-color="#6DC1ED" onEntry="this.borderRadius='25px'; this.boxShadow='unset'; this.fontWeight='800'; this.color='white';" width="150px" height="90px" string="${t.option4}" font-family="'Baloo 2', serif" font-size="24px" z="2" type="drag">
+                    <lido-text id="option4" value="${t.option4}" tab-index="8" flexible-width="false" visible="true" bg-color="#FCF3B1" onEntry="this.borderRadius='25px'; this.boxShadow='unset'; this.fontWeight='800'; " width="150px" height="90px" string="${t.option4}" font-family="'Baloo 2', serif" font-size="24px" z="2" type="drag">
                     </lido-text>
                 </lido-cell>
             </lido-cell>
