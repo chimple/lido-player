20e82fcf334f26e969493c92369bc508016dfd62
Santhosh Kumar S <santosh.kumar@sutara.org>
2025-07-28 14:55:17 +0530

    called position relative css from speak icon function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -814,7 +814,7 @@ export const speakIcon = (targetElement: HTMLElement) => {
     speakIcon.style.position = 'unset';
     return parentDiv;
   }
-
+  targetElement.style.position = "relative";
   return speakIcon;
 };
 
