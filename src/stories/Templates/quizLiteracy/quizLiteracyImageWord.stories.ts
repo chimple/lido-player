a86162a7259e778b8e04c54b8d6e81b040d492f5
nehacr7 <neha@sutara.org>
2025-08-22 10:42:20 +0530

    quizLiteracy

---
+++ src/stories/Templates/quizLiteracy/quizLiteracyImageWord.stories.ts
@@ -0,0 +1,100 @@
+import { Meta, StoryObj } from '@storybook/web-components';
+import { html } from 'lit';
+
+const meta: Meta = {
+  title: 'Templates/quizLiteracyImageWord',
+  argTypes: {
+ 
+  image: { control: 'file' },
+  },
+};
+export default meta;
+
+export const quizLiteracyImageWord: StoryObj = {
+  args: {
+   option1: "Elephant",
+   option2: "Fish",
+   option3: "Egg",
+   option4: "Nest",
+   answers:"Elephant",
+   image:['https://aeakbcdznktpsbrfsgys.supabase.co/storage/v1/object/public/template-assets/LetterPair/Cat.png'],
+  },
+
+  render: args => {
+    const xml = getContainerXml(args);
+    return html`<lido-home .xmlData="${xml}"></lido-home>`;
+  },
+};
+
+function getContainerXml(args) {
+    let tabCounter = 1;
+    const answers = Array.isArray(args.answers) ? args.answers : [args.answers];
+
+  return `<main>
+   <lido-container id="lido-container" value="maincontainer" visible="true" objective="${args.answers}"  bg-image="https://aeakbcdznktpsbrfsgys.supabase.co/storage/v1/object/public/template-assets/grid/Underwater.png"  onEntry="" is-Continue-On-Correct="true" onInCorrect="lido-avatar.avatarAnimate='Fail'; this.sleep='2000';" onCorrect="lido-avatar.avatarAnimate='Success'; this.sleep='2000';">
+	<lido-cell layout="pos" id="pos1" disable-edit="true" value="pos1" height="landscape.290px,portrait.476px" width="landscape.278px,portrait.467px" x="landscape.78%, portrait.24%" y="landscape.228px, portrait.66%" aria-hidden="true" z="2" bg-color="transparent" type="" visible="true" audio="" onTouch="" onCorrect="">
+		<lido-avatar id="lido-avatar" disable-edit="true" visible="true" height="100%" width="100%" src="https://aeakbcdznktpsbrfsgys.supabase.co/storage/v1/object/public/template-assets/temp1/chimplecharacter.riv" alt-text="{chimpleCharacterRive}" bg-color="transparent">
+		</lido-avatar>
+		<lido-image id="image1" disable-edit="true" value="image1" visible="true" src="https://aeakbcdznktpsbrfsgys.supabase.co/storage/v1/object/public/template-assets/temp1/Shadow.png" bg-color="transparent" width="165px" height="30px" x="landscape.54px,portrait.125px" y="landscape.239px,portrait.400px" alt-text="{shadowImgae}">
+		</lido-image>
+	</lido-cell>
+	<lido-cell layout="col" visible="true" bg-Color="transparent" width="90%" height="68%">
+		<lido-cell layout="flex" visible="true" width="100%" height="300px" bg-Color="transparent"
+  onEntry="this.align-items='center'; this.justify-content='center';">
+
+   ${answers
+    .filter(Boolean)
+    .map((answer) => {
+    if (!answer) return ""; // skip empty answers
+
+    const isImage = answer.startsWith("http") || /\.(png|jpg|jpeg|gif|webp|svg)$/i.test(answer);
+
+    if (!isImage) {
+      // Render text
+      return `
+        <lido-text visible="true" id="answer${tabCounter}"  
+          tab-index="${tabCounter++}" font-size="140px"
+          bg-color="#FFF5BB" border-radius="12px"
+          font-color="black" height="200px" width="auto"
+          string="${answer}" margin="landscape.0px,portrait.-131px 0px 0px 0px" value="${answer}" type="text"
+          onEntry="this.border='6px solid #FFB612';this.font-weight='500';this.padding='0px 64px';">
+        </lido-text>`;
+    } else {
+      // Render image
+      return `
+        <lido-cell visible="true" height="400px" width="400px" border-radius="7px"
+          bg-color="white" margin="landscape.-70px 0px 0px 0px, portrait.0px 0px 0px 0px"
+          onEntry="this.display='flex'; this.align-items='center'; this.justifyContent='center';">
+          <lido-image visible="true"
+            width="landscape.400px,portrait.400px"
+            height="landscape.400px,portrait.400px"
+            border-radius="7px" bg-color="white"
+            src="${answer}">
+          </lido-image>
+        </lido-cell>`;
+    }
+  })
+  .join("")
+}
+
+</lido-cell>
+
+
+		<lido-cell layout="flex" visible="true" margin="landscape.0px,portrait.-424px 0px 407px 0px" width="landscape.100%,portrait.100%" height="216px" bg-Color="transparent" onEntry="this.align-items='center'; this.justify-content='center'; this.gap='40px';">
+            
+            <lido-text visible="true" id="drag${tabCounter}" tab-index="${tabCounter++}" font-size="80px" border-radius="12px" font-color="black" height="148px" width="auto" string="${args.option1}" value="${args.option1}" type="click" onEntry="this.font-weight='500';this.padding='0px 64px 0px 64px';">
+            </lido-text>
+             <lido-text visible="true" id="drag${tabCounter}" tab-index="${tabCounter++}" font-size="80px" border-radius="12px" font-color="black" height="148px" width="auto" string="${args.option2}" value="${args.option2}" type="click" onEntry="this.font-weight='500';this.padding='0px 64px 0px 64px';">
+            </lido-text>
+             <lido-text visible="true" id="drag${tabCounter}" tab-index="${tabCounter++}" font-size="80px" border-radius="12px" font-color="black" height="148px" width="auto" string="${args.option3}" value="${args.option3}" type="click" onEntry="this.font-weight='500';this.padding='0px 64px 0px 64px';">
+            </lido-text>
+             <lido-text visible="true" id="drag${tabCounter}" tab-index="${tabCounter++}" font-size="80px" border-radius="12px" font-color="black" height="148px" width="auto" string="${args.option4}" value="${args.option4}" type="click" onEntry="this.font-weight='500';this.padding='0px 64px 0px 64px';">
+            </lido-text>
+            
+			
+		</lido-cell>
+
+	</lido-cell>
+</lido-container>
+  </main>`;
+}
