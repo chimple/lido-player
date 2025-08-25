989da45719905f0b914f87e7c87a873d97e57fbe
Bhanu <62737055+bhanugummana@users.noreply.github.com>
2025-08-25 17:04:12 +0530

    Reverted Assets loading offline support

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,8 +1,7 @@
 import type { StorybookConfig } from '@storybook/web-components-vite';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx|svg)'],
-  staticDirs: ['../src/components/home/'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: ['@storybook/addon-essentials', '@chromatic-com/storybook', '@storybook/experimental-addon-test'],
   framework: {
     name: '@storybook/web-components-vite',
