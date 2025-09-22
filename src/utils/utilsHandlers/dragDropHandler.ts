125666f2a5535b0a275f3ca82b8c0b9fc287fbba
Santhosh Kumar S <santosh.kumar@sutara.org>
2025-09-22 14:27:34 +0530

    Update storing score issue

--- src/utils/utilsHandlers/dragDropHandler.ts
+++ src/utils/utilsHandlers/dragDropHandler.ts
@@ -558,6 +558,7 @@ export async function onElementDropComplete(dragElement: HTMLElement, dropElemen
           }
         });
       }
+      storingEachActivityScore(false);
       return;
     } else {
       const checkdropAttr = container.getAttribute('dropAttr');
