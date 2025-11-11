524d664bc6cedd2b24838ff5aac863fc990700a2
GauravPahwa-SLF <gaurav.pahwa@sutara.org>
2025-11-11 13:51:37 +0530

    updated story

--- src/stories/Templates/multiplyBeeds/multiplyBeeds.stories.ts
+++ src/stories/Templates/multiplyBeeds/multiplyBeeds.stories.ts
@@ -38,7 +38,7 @@ function getContainerXml(args : MultiplyBeedsArgs) {
 
     return `
         <main>
-            <lido-container id="multiply-beads" disable_speak="true" show-drop-border="false" objective="${number}X${multiplicationFactor} = ${answer}" tab-index="1"  value="multiply-beads" bgImage="https://aeakbcdznktpsbrfsgys.supabase.co/storage/v1/object/public/template-assets/trace/Underwater.png"  height="100%" width="100%" bg-color="transparent"  visible="true" onCorrect="lido-avatar.avatarAnimate='Success'; this.sleep='2000';" onEntry="this.justifyContent='space-around'; this.MultiplyBeedsAnimation='true';" onInCorrect="lido-avatar.avatarAnimate='Fail'; this.sleep='2000';" show-check="false" is-continue-on-correct="true">
+            <lido-container id="multiply-beads" disable-speak="true" show-drop-border="false" objective="${number}X${multiplicationFactor} = ${answer}" tab-index="1"  value="multiply-beads" bgImage="https://aeakbcdznktpsbrfsgys.supabase.co/storage/v1/object/public/template-assets/trace/Underwater.png"  height="100%" width="100%" bg-color="transparent"  visible="true" onCorrect="lido-avatar.avatarAnimate='Success'; this.sleep='2000';" onEntry="this.justifyContent='space-around'; this.MultiplyBeedsAnimation='true';" onInCorrect="lido-avatar.avatarAnimate='Fail'; this.sleep='2000';" show-check="false" is-continue-on-correct="true">
 
                 <!-- Chimple Avatar -->
                 <lido-cell layout="pos" id="pos1" disableEdit="true" value="pos1" height="landscape.570px, portrait.700px" width="landscape.380px, portrait.485px" x="landscape.1045px, portrait.220px" y="landscape.525px, portrait.1075px" ariaHidden="true" bgColor="transparent" visible="true"  onEntry="this.flex-shrink='0'; this.z-index='0';">
